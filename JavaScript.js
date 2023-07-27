875d47b1fb356eccdb5b85112d017cbcb9bf7ddf
luyvannda <philips.joe329@gmail.com>
2023-07-27 11:07:43 +0700

    complete the game project. Hooray!!!

--- JavaScript.js
+++ JavaScript.js
@@ -8,6 +8,8 @@ function play(e) {
   const playerChoice = e.target.id;
   const computerChoice = getComputerChoice();
   const playRound = getGameScore(playerChoice, computerChoice);
+  const winner = getWinner();
+  const finalResult = displayWinner(winner);
 }
 
 function getComputerChoice() {
@@ -34,25 +36,66 @@ const cpuScore = document.querySelector(`#score p:last-child`);
 
 function getGameScore(player, cpu) {
   if (player === cpu) {
-    log(`You chose ${player}.
-    Computer chose ${cpu}.
+    log(`Computer chose ${cpu}.
     It is a tie, good luck next round!`);
   } else if (player === "rock" && cpu === "scissors"
     || player === "paper" && cpu === "rock"
     || player === "scissors" && cpu === "paper") {
-    log(`You chose ${player}.
-    Computer chose ${cpu}.
+    log(`Computer chose ${cpu}.
     You won this round!`);
     scoreBoard.player++;
   } else {
-    log(`You chose ${player}. 
-    Computer chose ${cpu}.
+    log(`Computer chose ${cpu}.
     You lose this round, come on pull yourself together!`);
     scoreBoard.computer++;
   }
 
   playerScore.textContent = `Player's Score: ${scoreBoard.player}`;
   cpuScore.textContent = `CPU's Score: ${scoreBoard.computer}`;
+}
+
+// Decide the winner after five round and display the result
+function getWinner() {
+  console.log(scoreBoard.player, scoreBoard.computer);
+  if (scoreBoard.player > scoreBoard.computer &&
+    scoreBoard.player === 5) {
+    return 'player';
+  } else if (scoreBoard.computer > scoreBoard.player &&
+    scoreBoard.computer === 5) {
+    return 'computer';
+  }
+}
+
+function displayWinner(winner) {
+  if (winner === 'player') {
+    result.innerHTML = `
+    <h1 class="text-win">You Win</h1>
+    <i class="modal-choice fa-solid fa-face-smile fa-8x"></i>
+    <p>Congratulations!!!</p>
+    `;
+    modal.style.display = `block`;
+  } else if (winner === 'computer') {
+    result.innerHTML = `
+    <h1 class="text-lose">You lose</h1>
+    <i class="modal-choice fa-solid fa-face-frown-open fa-8x"></i>
+    <p>Click outside to try Again!!!</p>
+    `;
+    modal.style.display = `block`;
+  }
+}
+
+// clear modal
+function clearModal(e) {
+  if (e.target == modal) {
+    modal.style.display = `none`;
+    scoreBoard.player = 0;
+    scoreBoard.computer = 0;
+    playerScore.textContent = `Player's Score: 0`;
+    cpuScore.textContent = `CPU's Score: 0`;
+
+    const hints = document.querySelector(".hints p");
+    hints.textContent = `Play the game for 5 rounds and test your skills against the cpu.`;
+  }
 }
 
 // Restart game 
@@ -70,66 +113,5 @@ function restartGame(e) {
 
 choices.forEach(choice => choice.addEventListener('click', play));
 restart.addEventListener('click', restartGame);
+window.addEventListener('click', clearModal);
 
-
-
-/*
-const choices = ["rock", "paper", "scissors"];
-
-let userScore = 0;
-let cpuScore = 0;
-
-function getComputerChoice() {
-  const randomIndex = Math.floor(Math.random() * choices.length);
-  const randomChoice = choices[randomIndex];
-  return randomChoice
-}
-
-function playRound() {
-  // your code here!
-  let round = 5;
-
-  for (let i = 1; i <= 5; i++) {
-    const computerSelection = getComputerChoice();
-    const playerSelection = prompt(`Please type rock, paper, or scissors`, "");
-    let playerLowerCase = playerSelection.toLowerCase();
-    if (choices.includes(playerLowerCase)) {
-      console.log(`What you chose: ${playerLowerCase}`);
-      console.log(`What computer chose: ${computerSelection}`);
-
-      if (playerLowerCase === computerSelection) {
-        console.log(`It is a tie, good luck next round`);
-      } else if (playerLowerCase === "rock" && computerSelection === "scissors"
-        || playerLowerCase === "paper" && computerSelection === "rock"
-        || playerLowerCase === "scissors" && computerSelection === "paper") {
-        console.log(`You won this round`);
-        userScore++;
-      } else {
-        console.log(`Computer won this round, come on pull yourself together`);
-        cpuScore++;
-      }
-    } else {
-      console.log("Wrong input, please type rock, paper, or scissors");
-    }
-  }
-  return userScore, cpuScore
-}
-
-
-function game(gameResult) {
-  console.log(`userScore = ${userScore}, cpuScore = ${cpuScore}`);
-  let finalResult;
-  if (userScore > cpuScore) {
-    finalResult = alert("You are the winner, human kind has been saved!!!");
-  } else if (userScore < cpuScore) {
-    finalResult = alert("You lost, computer will take over the world!!!");
-  } else if (userScore === cpuScore) {
-    finalResult = alert("It is a tie, try again or live together with the computer");
-  }
-
-  return finalResult
-}
- let gameResult = playRound();
- game(gameResult);
-
-*/
